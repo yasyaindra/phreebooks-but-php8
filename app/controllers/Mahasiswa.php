762d35295f9992584a6b4eaf31ef4dad0f0f22de
yasyaindra <indrayasya96@gmail.com>
2021-06-07 21:56:46 +0800

    solve the views

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -1,11 +1,11 @@
 <?php 
 
-class Home extends Controller {
+class Mahasiswa extends Controller {
     public function index(){
-        $data['judul'] = 'Home';
+        $data['judul'] = 'Daftar Mahasiswa';
         $data['nama'] = 'Indra';
         $this->view('templates/header', $data);
-        $this->view('home/index', $data);
+        $this->view('mahasiswa/index', $data);
         $this->view('templates/footer');
     }
 }
---
+++ app/views/mahasiswa/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+<div class="col">
+<div class="row">
+<ul>Daftar Mahasiswa
+<li>Nama :</li>
+<li>Jurusan :</li>
+<li>Email :</li>
+</ul>
+</div>
+</div>
+</div>
